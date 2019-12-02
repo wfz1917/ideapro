f0e9fa1269566fa8b7469f7f6b79bcbf07b26237
wfz <2396294824@qq.com>
2019-12-02 20:08:24 +0800

    初始化提交

---
+++ src/main/java/com/atguigu/nacosconsumer/NacosConsumerApplication.java
@@ -0,0 +1,15 @@
+package com.atguigu.nacosconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class NacosConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConsumerApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/atguigu/nacosconsumer/controller/ConsumerController.java
@@ -0,0 +1,20 @@
+package com.atguigu.nacosconsumer.controller;
+
+import com.atguigu.nacosconsumer.feign.ProviderClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConsumerController {
+
+    @Autowired
+    private ProviderClient providerClient;
+
+    @GetMapping("hi")
+    public String hi(){
+        String hello = this.providerClient.hello();
+        return "hi provider,===========" + hello;
+    }
+
+}
